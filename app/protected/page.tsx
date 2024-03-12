04a0915c04a59b286f4c38494723a158ce4723da
Vladyslav Tomchuk <tomchukvlad92@gmail.com>
2024-03-12 20:01:18 +0100

    chore: :zap: fix auth.

--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -1,11 +1,8 @@
-import DeployButton from "@/components/DeployButton";
 import AuthButton from "@/components/AuthButton";
 import { createClient } from "@/utils/supabase/server";
-import FetchDataSteps from "@/components/tutorial/FetchDataSteps";
-import Header from "@/components/Header";
 import { redirect } from "next/navigation";
 
-export default async function ProtectedPage() {
+export default async function UserHome() {
   const supabase = createClient();
 
   const {
@@ -13,19 +10,16 @@ export default async function ProtectedPage() {
   } = await supabase.auth.getUser();
 
   if (!user) {
-    return redirect("/login");
+    return redirect("/");
   }
+
+  const { data } = await supabase.from("clients").select("*");
 
   return (
     <div className="flex-1 w-full flex flex-col gap-20 items-center">
       <div className="w-full">
-        <div className="py-6 font-bold bg-purple-950 text-center">
-          This is a protected page that you can only see as an authenticated
-          user
-        </div>
         <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
           <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm">
-            {/* <DeployButton /> */}
             <div></div>
             <AuthButton />
           </div>
@@ -33,13 +27,7 @@ export default async function ProtectedPage() {
       </div>
 
       <div>ZEICE generetor</div>
-      {/* <div className="animate-in flex-1 flex flex-col gap-20 opacity-0 max-w-4xl px-3">
-        <Header />
-        <main className="flex-1 flex flex-col gap-6">
-          <h2 className="font-bold text-4xl mb-4">Next steps</h2>
-          <FetchDataSteps />
-        </main>
-      </div> */}
+      {data?.map((i) => i.bar_title)}
     </div>
   );
 }
